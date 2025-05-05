bfd9bc831041ed741188b8102af6b4811e22f1ac
GenghanWu <2983584w@student.gla.ac.uk>
2025-05-05 20:02:31 +0100

    Version1.0.1 2025.05.05 first commit

---
+++ src/main/java/com/wu/ailaw/AiLawApplication.java
@@ -0,0 +1,13 @@
+package com.wu.ailaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiLawApplication.class, args);
+	}
+
+}
